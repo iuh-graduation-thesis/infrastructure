6bcdbb4a86e28f43522c580055c6fdb8e332d585
nvminh162 <nvminh162@gmail.com>
2026-07-03 11:39:53 +0700

    initialize Keycloak event publisher project with Maven setup and basic application structure

---
+++ keycloak-event-publisher/src/main/java/com/fit/iuh/keycloak_event_publisher/KeycloakEventPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.fit.iuh.keycloak_event_publisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakEventPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakEventPublisherApplication.class, args);
+	}
+
+}
---
+++ keycloak-event-publisher/src/test/java/com/fit/iuh/keycloak_event_publisher/KeycloakEventPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fit.iuh.keycloak_event_publisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakEventPublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
